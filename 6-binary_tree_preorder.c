e551aa2dacafbff8a17953bf211595fa47f4bd13
flamonrose23 <fatinemar20@icloud.com>
2024-03-27 16:12:21 -0700

    Task 6 of pre-order transversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function of binary tree using pre-order traversal
+ * @tree: pointing to root of tree traversed
+ * @func: pointing to function to call for each node
+ * Return: Nothing if tree or func is NULL
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+}
