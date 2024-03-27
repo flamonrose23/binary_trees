f2d2215df551c778fa90a083c621a4e8f8838c9d
flamonrose23 <fatinemar20@icloud.com>
2024-03-26 20:40:17 -0700

    Task 3 Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - Writing function deleting entire binary tree
+ * @tree: pointing to the binary tree
+ *
+ * Return: Nothing if tree is NULL
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+		return;
+
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
