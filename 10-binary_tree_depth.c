431394735dfeed22ef04698ae0bb65b034035d1f
Imanegoubraim1 <imanegoubraim@gmail.com>
2024-03-27 17:58:01 -0700

    tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,5 @@
 size_t binary_tree_depth(const binary_tree_t *tree);
 
 {
-	if ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent));
-
-	return(0);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
