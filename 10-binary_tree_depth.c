50b2a33050897bda0f15f6122d327d0efb844c33
Imanegoubraim1 <imanegoubraim@gmail.com>
2024-03-27 17:55:48 -0700

    work

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree);
 
 {
-	if ((tree && tree->parent) + binary_tree_depth(tree->parent));
+	if ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent));
 
 	return(0);
 }
